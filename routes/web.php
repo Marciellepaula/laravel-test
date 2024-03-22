67090785e6ff29910cebf9c178e9e79698cb2b97
Marcielle <marcielle0644@gmail.com>
2024-03-22 01:30:34 -0300

    Organizando as rotas e adicionando midlleware

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\RelatorioController;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', fn () => view('welcome'));
 
 Auth::routes();
 
-Route::get('/dashboard', [App\Http\Controllers\RelatorioController::class, 'index'])->name('buscar');
+Route::middleware('auth')->group(function () {
+    Route::controller(FuncionarioController::class)->group(function () {
+        Route::get('/dashboard', 'index')->name('dashboard');
+        Route::get('/funcionarios/create', 'create')->name('funcionarios.create');
+        Route::post('/funcionarios', 'store')->name('funcionarios.store');
+        Route::get('/funcionarios/show/{id}', 'show')->name('funcionarios.show');
+        Route::get('/funcionarios/edit/{id}', 'edit')->name('funcionarios.edit');
+        Route::put('/funcionarios/update/{id}', 'update')->name('funcionarios.update');
+        Route::delete('/funcionarios/{id}', 'destroy')->name('funcionarios.destroy');
+    });
 
-Route::controller(FuncionarioController::class)->group(function () {
-    Route::get('/funcionarios', 'index')->name('funcionarios.index');
-    Route::get('/funcionarios/create', 'create')->name('funcionarios.create');
-    Route::post('/funcionarios', 'store')->name('funcionarios.store');
-    Route::get('/funcionarios/show/{id}', 'show')->name('funcionarios.show');
-    Route::get('/funcionarios/edit/{id}', 'edit')->name('funcionarios.edit');
-    Route::post('/funcionarios/update/{id}', 'update')->name('funcionarios.update');
-    Route::delete('/funcionarios/{id}', 'destroy')->name('funcionarios.destroy');
-});
-
-Route::controller('relatorios', 'RelatorioController')->group(function () {
-    Route::get('/relatorios/baixar', 'baixarRelatorio')->name('get-relatorios-baixar');
+    Route::controller(RelatorioController::class)->group(function () {
+        Route::get('/relatorios/baixar', 'baixarRelatorio')->name('get-relatorios-baixar');
+    });
 });
